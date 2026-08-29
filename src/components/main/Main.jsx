7aa604832e8eee348567e48285f4c2e3e470d3d2
Marisa Suares <smarisasofia@gmail.com>
2026-08-29 18:19:08 -0300

    hice cambios en main

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -8,9 +8,12 @@ export default function Main() {
           <h3 className={estilos.subtitulos}>Bienvenidos a</h3>
           <h1 className={estilos.titulo}>EL SISTEMA DE GESTION INTEGRAL</h1>
           <h3 className={estilos.subtitulos}>De Temple Estetique</h3>
+
+          <div className={estilos.linea}></div>
+
           <p className={estilos.descripcion}> Este es un sistema de gestion diseñado para organizar y controlar la informacion relacionada con la jornada laboral
             de los empleados de la estetica. 
-          <p>El mismo permitira registrar los horarios de entrada y salida de los empleados, llevar un control de los servicios realizados durante su jornada laboral y facilita el calculo del porcentaje de pago diario y aguindaldo correspondiente. </p>
+          <p className={estilos.descripcion}>El mismo permitira registrar los horarios de entrada y salida de los empleados, llevar un control de los servicios realizados durante su jornada laboral y facilita el calculo del porcentaje de pago diario y aguindaldo correspondiente. </p>
           </p>
         </main>
     </div>
